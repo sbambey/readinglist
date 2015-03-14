3b2215806fab0c4059ab7aceeb55ff41582dd69e
Simon Bambey <sbambey.lp@gmail.com>
2015-03-14 14:29:55 -0700

    Removes layout for channel.html file used by fb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+	layout false, only: [:channel]
   def contact
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
 
   get "list_items/retrieve_google_book/:title/:field_id", to: "list_items#retrieve_google_book"
 
-  resources :lists, path: '', only: [:show], as: "single_list"
-
   get "channel.html", to: "static_pages#channel"
+
+  resources :lists, path: '', only: [:show], as: "single_list"
 
   #get ":id", to: "lists#show"
 
